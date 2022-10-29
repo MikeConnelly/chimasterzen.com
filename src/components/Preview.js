cffcf95320dd0a4b44711754847e6db1d86e1c4e
Michael Connelly <mrc7197@gmail.com>
2022-10-29 12:22:39 -0400

    add about text

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -7,14 +7,14 @@ class Preview extends Component {
       <Modal
         id="preview-modal"
         open={this.props.open}>
-          <Box id="modal-contents">
-            <Box sx={{ height: 300, width: 250 }} component="img" src={this.props.item} />
-            <Typography>item title</Typography>
-            <Typography sx={{ color: 'green' }}>item price</Typography>
-            <Button>Add to Cart</Button>
-            <Button>Prev</Button>
-            <Button>Next</Button>
-          </Box>
+        <Box id="modal-contents">
+          <Box sx={{ height: 300, width: 250 }} component="img" src={this.props.item} />
+          <Typography>item title</Typography>
+          <Typography sx={{ color: 'green' }}>item price</Typography>
+          <Button>Add to Cart</Button>
+          <Button>Prev</Button>
+          <Button>Next</Button>
+        </Box>
       </Modal>
     );
   }
